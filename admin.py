e2b74f9f340dc2f8150d82628b1b0d4694ed5f3b
Mathieu Zen <zen.mathieu@gmail.com>
2018-11-08 10:04:32 +0100

    fix PEP8 admin

--- admin.py
+++ admin.py
@@ -28,7 +28,20 @@ from django.contrib import admin
 from continuing_education.models import admission, continuing_education_person,\
     address, file
 
-admin.site.register(admission.Admission, admission.AdmissionAdmin)
-admin.site.register(continuing_education_person.ContinuingEducationPerson, continuing_education_person.ContinuingEducationPersonAdmin)
-admin.site.register(address.Address, address.AddressAdmin)
-admin.site.register(file.File, file.FileAdmin)
+
+admin.site.register(
+    admission.Admission,
+    admission.AdmissionAdmin
+)
+admin.site.register(
+    continuing_education_person.ContinuingEducationPerson,
+    continuing_education_person.ContinuingEducationPersonAdmin
+)
+admin.site.register(
+    address.Address,
+    address.AddressAdmin
+)
+admin.site.register(
+    file.File,
+    file.FileAdmin
+)
