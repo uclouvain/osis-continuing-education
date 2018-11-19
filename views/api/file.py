ea0c69dba414e4b2c72f22497c9a7bfd13be2f2b
BenJ <benjamin.daubry@uclouvain.be>
2018-11-19 11:20:01 +0100

    IUFC-107 : add delete file to API

--- views/api/file.py
+++ views/api/file.py
@@ -25,6 +25,7 @@
 ##############################################################################
 from django.core import serializers
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from rest_framework import views, status
 from rest_framework.parsers import FileUploadParser, MultiPartParser
 from rest_framework.response import Response
@@ -63,6 +64,15 @@ class FileAPIView(views.APIView):
         return Response(
             data="File uploaded sucessfully",
             status=status.HTTP_201_CREATED
+        )
+
+    def delete(self, request):
+        file_path = request.query_params['file_path']
+        file = get_object_or_404(File, path=file_path)
+        file.delete()
+        return Response(
+            data="File deleted",
+            status=status.HTTP_204_NO_CONTENT
         )
 
 
