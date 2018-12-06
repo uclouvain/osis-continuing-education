a071a7f2dd14eff9cc3ba4f6bf085f6d5a20031b
BenJ <benjamin.daubry@uclouvain.be>
2018-12-06 09:21:00 +0100

    IUFC-API : API serializer Admission

---
+++ api/serializers/admission.py
@@ -0,0 +1,143 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from rest_framework import serializers
+
+from continuing_education.models.address import Address
+from continuing_education.models.admission import Admission
+from continuing_education.models.continuing_education_person import ContinuingEducationPerson
+from reference.models.country import Country
+
+
+class AdmissionSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='continuing_education_api_v1:admission-detail',
+        lookup_field='uuid'
+    )
+    person_information = serializers.SlugRelatedField(
+        slug_field='person',
+        queryset=ContinuingEducationPerson.objects.all()
+    )
+    citizenship = serializers.SlugRelatedField(
+        slug_field='citizenship',
+        queryset=Country.objects.all()
+    )
+    address = serializers.SlugRelatedField(
+        slug_field='address',
+        queryset=Address.objects.all()
+    )
+
+    # Display human readable value
+    professional_status_text = serializers.CharField(source='get_professional_status_display', read_only=True)
+    activity_sector_text = serializers.CharField(source='get_activity_sector_display', read_only=True)
+    state_text = serializers.CharField(source='get_state_display', read_only=True)
+    registration_type_text = serializers.CharField(source='get_registration_type_display', read_only=True)
+    marital_status_text = serializers.CharField(source='get_marital_status_display', read_only=True)
+
+    class Meta:
+        model = Admission
+        fields = (
+            'url',
+            'person_information',
+
+            # CONTACTS
+            'citizenship',
+            'address',
+            'phone_mobile',
+            'email',
+
+            # EDUCATION
+            'high_school_diploma',
+            'high_school_graduation_year',
+            'last_degree_level',
+            'last_degree_field',
+            'last_degree_institution',
+            'last_degree_graduation_year',
+            'other_educational_background',
+
+            # PROFESSIONAL BACKGROUND
+            'profesionnal_status',
+            'professional_status_text'
+            'current_occupation',
+            'current_employer',
+            'activity_sector',
+            'activity_sector_text',
+            'past_professional_activities',
+
+            # MOTIVATION
+            'motivation',
+            'professional_impact',
+            'formation',
+
+            #AWARENESS
+            'awareness_ucl_website',
+            'awareness_formation_website',
+            'awareness_press',
+            'awareness_facebook',
+            'awareness_linkedin',
+            'awareness_customized_mail',
+            'awareness_emailing',
+            'awareness_other',
+
+            'state',
+            'state_text',
+
+            # REGISTRATION
+            # BILLING
+            'registration_type',
+            'registration_type_text'
+            'use_address_for_billing',
+            'billing_address',
+            'head_office_name',
+            'company_number',
+            'vat_number',
+
+            # REGISTRATION
+            'national_registry_number',
+            'id_card_number',
+            'passport_number',
+            'marital_status',
+            'marital_status_text',
+            'spouse_name'
+            'children_number',
+            'previous_ucl_registration',
+            'previous_noma',
+
+            # POST
+            'use_address_for_post',
+            'residence_address',
+            'residence_phone',
+
+            # STUDENT SHEET
+            'registration_complete',
+            'noma',
+            'payment_complete',
+            'formation_spreading',
+            'prior_experience_validation',
+            'assessment_presented',
+            'assessment_succeeded',
+            'sessions'
+
+        )
