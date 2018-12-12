7d083a3268721fcca258ffb0eb93e44290189f33
Mathieu Zen <zen.mathieu@gmail.com>
2018-12-12 09:37:18 +0100

    IUFC-160 add state_reason field to model

--- models/admission.py
+++ models/admission.py
@@ -198,6 +198,11 @@ class Admission(SerializableModel):
         verbose_name=_("State")
     )
 
+    state_reason = models.TextField(
+        blank=True,
+        verbose_name=_("Motivation")
+    )
+
     # Billing
     registration_type = models.CharField(
         max_length=50,
