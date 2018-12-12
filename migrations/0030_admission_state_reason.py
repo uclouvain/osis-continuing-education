fdd57c3acd3ab5a29ce982b287349f78588f707d
Mathieu Zen <zen.mathieu@gmail.com>
2018-12-12 16:07:56 +0100

    IUFC-160 fix conflicting migrations

--- migrations/0030_admission_state_reason.py
+++ migrations/0030_admission_state_reason.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('continuing_education', '0028_auto_20181204_1422'),
+        ('continuing_education', '0029_auto_20181212_1040'),
     ]
 
     operations = [
