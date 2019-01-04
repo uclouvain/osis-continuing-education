60fe3d3e835bc29f6e5d41dae57fa3a004d8a87b
lspitaels <laurent.spitaels@uclouvain.be>
2019-01-04 15:21:11 +0100

    IUFC-179 handle upload file from admission detail view

--- views/admission.py
+++ views/admission.py
@@ -36,6 +36,7 @@ from base.models import entity_version
 from base.models.education_group_year import EducationGroupYear
 from base.models.entity_version import EntityVersion
 from base.models.enums import entity_type
+from base.models.person import Person
 from continuing_education.forms.account import ContinuingEducationPersonForm
 from continuing_education.forms.address import AddressForm
 from continuing_education.forms.admission import AdmissionForm
@@ -102,6 +103,19 @@ def admission_detail(request, admission_id):
         request.POST or None,
         instance=admission,
     )
+
+    if request.method == 'POST' and request.FILES:
+        my_file = request.FILES['myfile']
+        person = Person.objects.get(user=request.user)
+        file_to_admission = File(
+            admission=admission,
+            path=my_file,
+            name=my_file.name,
+            size=my_file.size,
+            uploaded_by=person
+        )
+        file_to_admission.save()
+        return redirect(reverse('admission_detail', kwargs={'admission_id': admission.pk}))
     if adm_form.is_valid():
         adm_form.save()
         return redirect(reverse('admission_detail', kwargs={'admission_id': admission.pk}))
