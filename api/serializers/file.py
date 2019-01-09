44950f73b643146b485fdc4e19381b5b92c84934
BenJ <benjamin.daubry@uclouvain.be>
2019-01-09 15:38:53 +0100

    test override url

--- api/serializers/file.py
+++ api/serializers/file.py
@@ -24,17 +24,33 @@
 #
 ##############################################################################
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from base.api.serializers.person import PersonDetailSerializer
 from continuing_education.models.file import File
 
 
-class FileSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name='continuing_education_api_v1:file-detail',
-        lookup_field='uuid',
-        lookup_url_kwarg='file_uuid'
-    )
+class FileHyperlink(serializers.HyperlinkedRelatedField):
+    view_name = 'continuing_education_api_v1:file-detail'
+    queryset = File.objects.all()
+
+    def get_url(self, obj, view_name, request, format):
+        url_kwargs = {
+            'uuid': obj.admission.uuid,
+            'file_uuid': obj.uuid
+        }
+        return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
+
+    def get_object(self, view_name, view_args, view_kwargs):
+        lookup_kwargs = {
+           'uuid': view_kwargs['file_uuid'],
+           'admission__uuid': view_kwargs['uuid']
+        }
+        return self.get_queryset().get(**lookup_kwargs)
+
+
+class FileSerializer(serializers.ModelSerializer):
+    url = FileHyperlink
     uploaded_by = PersonDetailSerializer()
     created_date = serializers.DateTimeField()
 
