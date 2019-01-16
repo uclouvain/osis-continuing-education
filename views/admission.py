06c90325039e9ae315660ddf665e6be10bbeee3e
Mathieu Zen <zen.mathieu@gmail.com>
2019-01-16 15:50:41 +0100

    IUFC-210 guess content_type based on file name

--- views/admission.py
+++ views/admission.py
@@ -24,6 +24,7 @@
 #
 ##############################################################################
 import itertools
+import mimetypes
 from datetime import datetime
 
 from django.contrib.auth.decorators import login_required, permission_required
@@ -159,7 +160,7 @@ def _upload_file(request, admission):
 def download_file(request, admission_id, file_id):
     file = File.objects.get(pk=file_id)
     filename = file.name.split('/')[-1]
-    response = HttpResponse(file.path, content_type='text/plain')
+    response = HttpResponse(file.path, content_type=mimetypes.guess_type(filename))
     response['Content-Disposition'] = 'attachment; filename=%s' % filename
     return response
 
