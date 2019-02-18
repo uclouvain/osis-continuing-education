8063095e6e158a7c11c5c90fe05544d519680f0d
BenJ <benjamin.daubry@uclouvain.be>
2019-02-18 11:14:00 +0100

    Remove self from method

--- api/common.py
+++ api/common.py
@@ -1,7 +1,7 @@
 from continuing_education.models.address import Address
 
 
-def update_address(self, instance, validated_data, address_field):
+def update_address(instance, validated_data, address_field):
     if address_field in validated_data:
         address_data = validated_data.pop(address_field)
         address, created = Address.objects.update_or_create(**address_data)
