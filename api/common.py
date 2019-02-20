4618a36690dcc70fe504ea6886968ab041a242e0
BenJ <benjamin.daubry@uclouvain.be>
2019-02-20 10:24:03 +0100

    Remove common

--- api/common.py
+++
@@ -1,8 +0,0 @@
-from continuing_education.models.address import Address
-
-
-def update_address(instance, validated_data, address_field):
-    if address_field in validated_data:
-        address_data = validated_data.pop(address_field)
-        address, created = Address.objects.update_or_create(**address_data)
-        setattr(instance, address_field, address)
