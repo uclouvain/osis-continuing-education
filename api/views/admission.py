d6d188fca01eca52e4403afa23b4bd6d415161dd
BenJ <benjamin.daubry@uclouvain.be>
2019-02-20 10:37:04 +0100

    Remove useless lines

--- api/views/admission.py
+++ api/views/admission.py
@@ -24,7 +24,6 @@
 #
 ##############################################################################
 from rest_framework import generics
-from rest_framework.response import Response
 
 from continuing_education.api.serializers.admission import AdmissionDetailSerializer, \
     AdmissionListSerializer, AdmissionPostSerializer
@@ -95,18 +94,3 @@ class AdmissionDetailUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
         if self.request.method == 'PUT' or self.request.method == 'PATCH':
             return AdmissionPostSerializer
         return AdmissionDetailSerializer
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=False)
-        print(serializer.errors)
-        self.perform_update(serializer)
-
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return Response(serializer.data)
