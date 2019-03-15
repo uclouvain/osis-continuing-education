2d8eb4ee3f72324fb6c4dbe6a48ca64c72c6e601
BenJ <benjamin.daubry@uclouvain.be>
2019-03-15 12:04:03 +0100

    Add tests

--- tests/business/test_admission.py
+++ tests/business/test_admission.py
@@ -23,14 +23,21 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+from unittest.mock import patch
+
+from django.contrib.sites.models import Site
 from django.test import TestCase
+from django.urls import reverse
 from django.utils.translation import gettext as _
 
 from base.tests.factories.academic_year import AcademicYearFactory
 from base.tests.factories.education_group import EducationGroupFactory
 from base.tests.factories.education_group_year import EducationGroupYearFactory
+from base.tests.factories.group import GroupFactory
 from base.tests.factories.person import PersonFactory
-from continuing_education.business.admission import _get_formatted_admission_data, _get_managers_mails
+from continuing_education.business import admission
+from continuing_education.business.admission import _get_formatted_admission_data, _get_managers_mails, \
+    CONTINUING_EDUCATION_MANAGERS_GROUP
 from continuing_education.tests.factories.admission import AdmissionFactory
 from continuing_education.tests.factories.continuing_education_training import ContinuingEducationTrainingFactory
 from continuing_education.tests.factories.person_training import PersonTrainingFactory
@@ -83,3 +90,118 @@ class TestAdmission(TestCase):
         expected_mails = "{} {}{} ".format(manager.email, _(" or "), manager_2.email)
 
         self.assertEqual(_get_managers_mails(admission.formation), expected_mails)
+
+
+class SendEmailTest(TestCase):
+    def setUp(self):
+        ed = EducationGroupFactory()
+        EducationGroupYearFactory(education_group=ed)
+        self.manager = PersonFactory(last_name="AAA")
+        manager_2 = PersonFactory(last_name="BBB")
+        cet = ContinuingEducationTrainingFactory(education_group=ed)
+        PersonTrainingFactory(person=self.manager, training=cet)
+        PersonTrainingFactory(person=manager_2, training=cet)
+        self.admission = AdmissionFactory(formation=cet)
+
+    @patch('continuing_education.business.admission.send_email')
+    def test_send_state_changed_email(self, mock_send):
+        admission.send_state_changed_email(self.admission)
+        args = mock_send.call_args[1]
+
+        self.assertEqual(_(self.admission.state), args.get('subject_data').get('state'))
+        self.assertEqual(
+            _get_managers_mails(self.admission.formation),
+            args.get('template_data').get('mails')
+        )
+        self.assertEqual(
+            self.admission.person_information.person.first_name,
+            args.get('template_data').get('first_name')
+        )
+        self.assertEqual(
+            self.admission.person_information.person.last_name,
+            args.get('template_data').get('last_name')
+        )
+        self.assertEqual(
+            self.admission.formation,
+            args.get('template_data').get('formation')
+        )
+        self.assertEqual(
+            _(self.admission.state),
+            args.get('template_data').get('state')
+        )
+        self.assertEqual(
+            self.admission.state_reason if self.admission.state_reason else "-",
+            args.get('template_data').get('reason')
+        )
+        self.assertEqual(len(args.get('receivers')), 1)
+        self.assertIsNone(args.get('attachment'))
+
+    @patch('continuing_education.business.admission.send_email')
+    def test_send_admission_submitted_email_to_admin(self, mock_send):
+        group = GroupFactory(name=CONTINUING_EDUCATION_MANAGERS_GROUP)
+        self.manager.user.groups.add(group)
+        admission.send_admission_submitted_email_to_admin(self.admission)
+        args = mock_send.call_args[1]
+        self.assertEqual(_(self.admission.formation.acronym), args.get('subject_data').get('formation'))
+        self.assertEqual(
+            self.admission.person_information.person.first_name,
+            args.get('template_data').get('first_name')
+        )
+        self.assertEqual(
+            self.admission.person_information.person.last_name,
+            args.get('template_data').get('last_name')
+        )
+        self.assertEqual(
+            self.admission.formation,
+            args.get('template_data').get('formation')
+        )
+        self.assertEqual(
+            _(self.admission.state),
+            args.get('template_data').get('state')
+        )
+        relative_path = reverse('admission_detail', kwargs={'admission_id': self.admission.id})
+        url = 'https://{}{}'.format(Site.objects.get_current().domain, relative_path)
+        self.assertEqual(
+            url,
+            args.get('template_data').get('formation_link')
+        )
+        self.assertEqual(len(args.get('receivers')), 1)
+        self.assertIsNone(args.get('attachment'))
+
+    @patch('continuing_education.business.admission.send_email')
+    def test_send_admission_submitted_email_to_participant(self, mock_send):
+        admission.send_admission_submitted_email_to_participant(self.admission)
+        args = mock_send.call_args[1]
+
+        self.assertEqual({}, args.get('subject_data'))
+        self.assertEqual(
+            _get_managers_mails(self.admission.formation),
+            args.get('template_data').get('mails')
+        )
+        self.assertEqual(
+            self.admission.formation.acronym,
+            args.get('template_data').get('formation')
+        )
+        self.assertEqual(
+            _get_formatted_admission_data(self.admission),
+            args.get('template_data').get('admission_data')
+        )
+        self.assertEqual(len(args.get('receivers')), 1)
+        self.assertIsNone(args.get('attachment'))
+
+    @patch('continuing_education.business.admission.send_email')
+    def test_send_invoice_uploaded_email(self, mock_send):
+        admission.send_invoice_uploaded_email(self.admission)
+        args = mock_send.call_args[1]
+
+        self.assertEqual({}, args.get('subject_data'))
+        self.assertEqual(
+            _get_managers_mails(self.admission.formation),
+            args.get('template_data').get('mails')
+        )
+        self.assertEqual(
+            self.admission.formation.acronym,
+            args.get('template_data').get('formation')
+        )
+        self.assertEqual(len(args.get('receivers')), 1)
+        self.assertIsNone(args.get('attachment'))
