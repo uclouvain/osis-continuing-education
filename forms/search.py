1439df8b9f76cf65399d31de9bfe7f72e943d8b6
Mathieu GERARD <mathieu.gerard@uclouvain.be>
2019-03-19 08:54:03 +0100

    IUFC-320 Display field training_aid in formations list

--- forms/search.py
+++ forms/search.py
@@ -248,9 +248,7 @@ def _get_filter_entity_management(qs, requirement_entity_acronym, with_entity_su
 
 class FormationFilterForm(AdmissionFilterForm):
     acronym = forms.CharField(max_length=40, required=False, label=_('Acronym'))
-
     title = forms.CharField(max_length=50, required=False, label=_('Title'))
-
     state = forms.ChoiceField(choices=FORMATION_STATE_CHOICES, required=False, label=_('State'))
 
     def get_formations(self):
@@ -279,7 +277,7 @@ class FormationFilterForm(AdmissionFilterForm):
         if title:
             qs = qs.filter(educationgroupyear__title__icontains=title)
 
-        return qs.order_by('educationgroupyear__acronym').distinct()
+        return qs.order_by('educationgroupyear__acronym').select_related('continuingeducationtraining').distinct()
 
 
 def _build_active_parameter(qs, state):
