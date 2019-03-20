40f902dcbc8e86fa513030d2625aefbdb0b9e5be
Mathieu GERARD <mathieu.gerard@uclouvain.be>
2019-03-20 15:35:11 +0100

    IUFC-335 Multiple change training_aid

--- static/js/formation.js
+++ static/js/formation.js
@@ -11,3 +11,13 @@ $("#btn_disable").click(function(e) {
     $("#new_state").val(false);
     $("#formations_form").submit();
 });
+
+$("#btn_training_aid_yes").click(function(e) {
+    $("#new_training_aid_value").val(true);
+    $("#formations_form").submit();
+});
+
+$("#btn_training_aid_no").click(function(e) {
+    $("#new_training_aid_value").val(false);
+    $("#formations_form").submit();
+});
