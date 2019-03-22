cae688cde95d04d1157f5ec51af8cda8d91de3e2
Mathieu GERARD <mathieu.gerard@uclouvain.be>
2019-03-22 14:33:43 +0100

    IUFC-335 Fix bug when no value in js submit

--- static/js/formation.js
+++ static/js/formation.js
@@ -4,20 +4,24 @@ $("#id_check_all").click(function(){
 
 $("#btn_activate").click(function(e) {
     $("#new_state").val("True");
+    $("#new_training_aid_value").val("None");
     $("#formations_form").submit();
 });
 
 $("#btn_disable").click(function(e) {
     $("#new_state").val("False");
+    $("#new_training_aid_value").val("None");
     $("#formations_form").submit();
 });
 
 $("#btn_training_aid_yes").click(function(e) {
     $("#new_training_aid_value").val("True");
+    $("#new_state").val("None");
     $("#formations_form").submit();
 });
 
 $("#btn_training_aid_no").click(function(e) {
     $("#new_training_aid_value").val("False");
+    $("#new_state").val("None");
     $("#formations_form").submit();
 });
