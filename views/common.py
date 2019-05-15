f622f606c2a3d35122a36a9eb34a259d69ceab9c
BenJ <benjamin.daubry@uclouvain.be>
2019-05-15 13:04:20 +0200

    Same

--- views/common.py
+++ views/common.py
@@ -118,8 +118,6 @@ def _get_appropriate_revision_message(form):
         messages.append(_get_messages(messages, UCL_REGISTRATION_COMPLETE))
     if 'registration_file_received' in form.changed_data and form.cleaned_data['registration_file_received']:
         messages.append(_get_messages(messages, REGISTRATION_FILE_RECEIVED))
-    if 'archived' in form.changed_data and form.cleaned_data['archived']:
-        messages.append(_get_messages(messages, FILE_ARCHIVED))
     message = ' '.join(messages) if messages else ''
     return message
 
