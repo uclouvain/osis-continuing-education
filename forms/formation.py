76c412ebf6afe1f1f269e84e4cfffa834e250e00
BenJ <benjamin.daubry@uclouvain.be>
2019-05-29 15:20:47 +0200

    IUFC-466 : Create form and template for form

---
+++ forms/formation.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
+
+from continuing_education.models.continuing_education_training import ContinuingEducationTraining
+
+
+class ContinuingEducationTrainingForm(ModelForm):
+
+    class Meta:
+        model = ContinuingEducationTraining
+        fields = [
+            'training_aid',
+            'active',
+            'postal_address',
+            'education_group'
+        ]
+
+        # Automatic translation of field names
+        labels = {field: _(field) for field in fields}
