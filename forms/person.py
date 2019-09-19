7e069c7cc93939ef9492c130ad6a1330c779d86b
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2019-09-19 08:40:21 +0200

    #IUFC-507 Enlever la valeur '--------' de la liste déroulante du genre

--- forms/person.py
+++ forms/person.py
@@ -1,3 +1,29 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2019 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from django import forms
 from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
 
@@ -10,13 +36,18 @@ ADMISSION_PARTICIPANT_REQUIRED_FIELDS = [
 
 
 class PersonForm(ModelForm):
+    gender = forms.ChoiceField(
+        choices=Person.GENDER_CHOICES,
+    )
 
     def __init__(self, *args, **kwargs):
+
         super(PersonForm, self).__init__(*args, **kwargs)
 
         set_participant_required_fields(self.fields,
                                         ADMISSION_PARTICIPANT_REQUIRED_FIELDS,
                                         True)
+        self.fields['gender'].initial = Person.GENDER_CHOICES[2]
 
     class Meta:
         model = Person
