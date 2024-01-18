00108e4d8c9ded956b56a81068d2b8c342314a50
Christophe Fivet <christophe.fivet@uclouvain.be>
2024-01-18 14:51:16 +0100

    Remove Warning log

--- api/views/continuing_education_person.py
+++ api/views/continuing_education_person.py
@@ -76,7 +76,6 @@ class ContinuingEducationPersonDetail(generics.RetrieveAPIView):
             logger.warning(self.request.user.username)
             return ContinuingEducationPerson.objects.get(person__user=self.request.user)
         except ContinuingEducationPerson.DoesNotExist:
-            logger.warning(Person.objects.get(email=self.request.user.username).id)
             person, _ = Person.objects.get_or_create(email=self.request.user.username)
             person.user = self.request.user
             person.save()
